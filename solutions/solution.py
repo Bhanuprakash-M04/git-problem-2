243e15a752d10392366b98e7ec55e76ef58383ed
Bhanu Prakash <bhanuprakash9501@gmail.com>
2025-05-31 14:35:31 +0530

    check python solution wrong

--- solutions/solution.py
+++ solutions/solution.py
@@ -3,5 +3,4 @@ from typing import List
 #solution
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums) - k]
+        pass
